c87b44f8364d1d26df6f3ea0eed8d138f09116ff
Kostiuk Dima <1dima.kostyk@gmail.com>
2019-07-25 22:20:11 +0300

    Struct

--- Game/Game/Object.h
+++ Game/Game/Object.h
@@ -7,6 +7,20 @@ private:
 
 public:
 	Object(){}
-	~Object(){}
+
+	virtual int GetPower() = 0;
+	
+	virtual int GetHealth() = 0;
+	
+	virtual void SetPower(int power) = 0;
+	
+	virtual void SetHealth(int health) = 0;
+	
+	virtual	void Print() = 0;
+
+	virtual int GetCount() = 0;
+	
+	virtual void SetCount(int  count) = 0;
+	
 };
 
