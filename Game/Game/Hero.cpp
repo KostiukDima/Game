73668a0daa9a83210c10a3dcea3bc8f27081a2b8
Kostiuk Dima <1dima.kostyk@gmail.com>
2019-08-12 01:11:32 +0300

    Add Deer

--- Game/Game/Hero.cpp
+++ Game/Game/Hero.cpp
@@ -2201,6 +2201,318 @@ void Tree::Print(int x, int y)
 
 void Deer::Print(int x, int y)
 {
+	HWND hW = GetConsoleWindow();
+
+	int xBegin = x;
+	int yBegin = y;
+	int yEnd;
+
+	for (int i = 0; i < 22; i++)
+	{
+		switch (i)
+		{
+		case 0:
+
+			yBegin = y + 65;
+			yEnd = y + 72;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 1:
+
+
+			yBegin = y + 59;
+			yEnd = y + 67;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 71;
+			yEnd = y + 72;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 2:
+
+			yBegin = y + 57;
+			yEnd = y + 62;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 3:
+
+			yBegin = y + 37;
+			yEnd = y + 44;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 55;
+			yEnd = y + 60;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 63;
+			yEnd = y + 72;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 4:
+			yBegin = y + 41;
+			yEnd = y + 58;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 61;
+			yEnd = y + 62;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 71;
+			yEnd = y + 72;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 5:
+			yBegin = y + 9;
+			yEnd = y + 14;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 43;
+			yEnd = y + 56;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 59;
+			yEnd = y + 60;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 6:
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 41;
+			yEnd = y + 54;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 57;
+			yEnd = y + 58;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 7:
+
+			yBegin = y + 17;
+			yEnd = y + 18;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 39;
+			yEnd = y + 52;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 8:
+			yBegin = y + 3;
+			yEnd = y + 6;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 39;
+			yEnd = y + 46;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 49;
+			yEnd = y + 50;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 9:
+			yBegin = y + 7;
+			yEnd = y + 12;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 39;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 10:
+
+			yBegin = y + 3;
+			yEnd = y + 6;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 13;
+			yEnd = y + 14;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 37;
+			yEnd = y + 50;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 11:
+
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 37;
+			yEnd = y + 50;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 12:
+
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 23;
+			yEnd = y + 24;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 35;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 13:
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 44;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 47;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+		case 14:
+
+			yBegin = y + 5;
+			yEnd = y + 8;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 17;
+			yEnd = y + 46;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 15:
+
+			yBegin = y + 9;
+			yEnd = y + 14;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+
+			yBegin = y + 17;
+			yEnd = y + 20;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+
+			yBegin = y + 23;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 16:
+
+			yBegin = y + 7;
+			yEnd = y + 8;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 19;
+			yEnd = y + 26;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 29;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 17:
+
+			yBegin = y + 15;
+			yEnd = y + 16;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 21;
+			yEnd = y + 26;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 33;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		case 18:
+			yBegin = y + 21;
+			yEnd = y + 26;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 35;
+			yEnd = y + 42;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 47;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 55;
+			yEnd = y + 56;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+
+			xBegin += 2;
+
+			break;
+		case 19:
+			yBegin = y + 23;
+			yEnd = y + 24;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 47;
+			yEnd = y + 48;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 50;
+			yEnd = y + 53;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 20:
+			yBegin = y + 47;
+			yEnd = y + 50;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			yBegin = y + 59;
+			yEnd = y + 60;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+		case 21:
+			yBegin = y + 49;
+			yEnd = y + 58;
+			Line(hW, xBegin, yBegin, xBegin, yEnd, RGB(250, 250, 250));
+			xBegin += 2;
+
+			break;
+
+		default:
+			break;
+		}
+
+	}
 }
 
 void Bush::Print(int x, int y)
